e7051316506017ed77bc72a43e7e4dff32bb3585
sujatha.kannan <sujatha.kannan@logiball.de>
2022-04-07 13:49:36 +0200

    Testing commit from local IDE Checking if Jenkins integration works

--- src/main/java/com/sujatha/example/controller/AppController.java
+++ src/main/java/com/sujatha/example/controller/AppController.java
@@ -7,12 +7,17 @@ import org.springframework.web.servlet.ModelAndView;
 @RestController
 public class AppController {
 	
-	@RequestMapping("/home")
+	/*@RequestMapping("/home")
 	public ModelAndView getHomePage() {
 		ModelAndView mv = new ModelAndView();
 	    mv.setViewName("home");
 		mv.addObject("hello", "Hi. I am a rest microservice");
 		return mv;
 	}
+*/
 
+	@RequestMapping("/home")
+	public String getMain(){
+		return "Hello. I am the first microservice in your webapp";
+	}
 }
